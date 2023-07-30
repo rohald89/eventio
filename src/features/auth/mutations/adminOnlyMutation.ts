072590ab4d2dd87714b42315886a8400ba972139
Rohald van Merode <rohald_89@live.nl>
2023-07-30 10:52:23 +0200

    fixed ts issue

--- src/features/auth/mutations/adminOnlyMutation.ts
+++ src/features/auth/mutations/adminOnlyMutation.ts
@@ -1,15 +1,12 @@
 import { resolver } from "@blitzjs/rpc";
-import db from "db";
 import { z } from "zod";
 
-const Input = z.object({
-  //   id: z.string(),
-});
+const Input = z.object({});
 
 export default resolver.pipe(
   resolver.zod(Input),
   resolver.authorize("ADMIN"),
-  async ({ id }, { session: { userId } }) => {
+  async ({}, { session: { userId } }) => {
     console.log("only admin can do this");
   }
 );
