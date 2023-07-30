50dd484b50ff7252a61a0e5953a02fc9bcdeae93
Rohald van Merode <rohald_89@live.nl>
2023-07-30 21:52:29 +0200

    email verification

--- src/pages/auth/verify-email.page.tsx
+++ src/pages/auth/verify-email.page.tsx
@@ -1,16 +1,31 @@
 import Layout from "@/core/layouts/Layout";
-import { useStringParam, useStringQueryParam } from "@/utils/utils";
+import verifyEmailToken from "@/features/auth/queries/verifyEmailToken";
+import { useStringQueryParam } from "@/utils/utils";
 import { BlitzPage } from "@blitzjs/next";
+import { useQuery } from "@blitzjs/rpc";
 import { Text } from "@mantine/core";
 import { Vertical } from "mantine-layout-components";
-import { useRouter } from "next/router";
 
 export const VerifyEmailPage: BlitzPage = () => {
   const token = useStringQueryParam("token");
+
+  const [result, { isSuccess, error }] = useQuery(
+    verifyEmailToken,
+    {
+      token: token as any,
+    },
+    {
+      enabled: !!token,
+    }
+  );
+
   return (
     <Layout>
       <Vertical>
-        <Text>Token is {token} </Text>
+        <>
+          {result && isSuccess && <Text>Email Verified</Text>}
+          {error && <Text>Invalid Token</Text>}
+        </>
       </Vertical>
     </Layout>
   );
