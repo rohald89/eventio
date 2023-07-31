dbec9f7906a58f483122ee014ced18d037f7d1b9
Rohald van Merode <rohald_89@live.nl>
2023-07-31 10:48:07 +0200

    fixed import

--- src/features/auth/mutations/login.ts
+++ src/features/auth/mutations/login.ts
@@ -1,9 +1,9 @@
 import { resolver } from "@blitzjs/rpc";
 import { Role } from "types";
 import { authenticateUser } from "@/utils/auth";
-import { Login } from "../schemas";
+import { LoginInput } from "../schemas";
 
-export default resolver.pipe(resolver.zod(Login), async (params, ctx) => {
+export default resolver.pipe(resolver.zod(LoginInput), async (params, ctx) => {
   const { email, password } = params;
   const user = await authenticateUser(email, password);
   await ctx.session.$create({ userId: user.id, role: user.role as Role });
