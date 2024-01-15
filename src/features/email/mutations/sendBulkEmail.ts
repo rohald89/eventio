f69f8c9db65c47b012af0e764b19af62d9edff97
Rohald van Merode <rohald_89@live.nl>
2024-01-15 13:38:28 +0100

    setup query for users matching the email list and split them up in chunks

--- src/features/email/mutations/sendBulkEmail.ts
+++ src/features/email/mutations/sendBulkEmail.ts
@@ -6,6 +6,8 @@ import { z } from "zod";
 import React from "react";
 import { generateUnsubscribeLink } from "@/utils/email";
 import { EmailList } from "@/features/email/types";
+import { chunk } from "lodash";
+import { isDev } from "@/config";
 
 const Input = z.object({
   list: z.nativeEnum(EmailList)
@@ -22,6 +24,27 @@ export default resolver.pipe(
     if (!user) throw new Error("User not found");
 
     console.log('list is', list);
+
+    const users = await db.user.findMany({
+      where: {
+        AND: [
+          {
+            ...list === EmailList.Product && { settingsEmailProduct: true },
+          },
+          {
+            ...list === EmailList.Marketing && { settingsEmailMarketing: true },
+          }, {
+          id: { not: user.id }
+          }
+        ]
+      },
+    })
+
+
+    const CHUNK_SIZE = isDev ? 3 : 100;
+    const chunks = chunk(users, CHUNK_SIZE)
+
+    console.log('chunks', chunks.length)
 
     // const unsubscribeLink = await generateUnsubscribeLink({
     //   userId: user.id,
